107d084465d8a34e4c5acd11d09dacd253b6c737
ArchismwanChatterjee <archismwancmail@gmail.com>
2025-06-26 14:59:06 +0530

    update metadata for Code Playground

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Code Playground',
+  description: 'Created with tech',
+  generator: 'dev arch',
 }
 
 export default function RootLayout({
